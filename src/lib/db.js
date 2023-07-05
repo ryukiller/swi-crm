659dc3a36744ac60b10d8635dcc643abcfc54549
ryukiller <rian.rodrigo.webmaster@gmail.com>
2023-07-05 14:13:31 +0200

    updating

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,10 @@
 import { createPool } from 'mysql2/promise';
 
 const pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB,
+  host: process.env.PS_HOST,
+  user: process.env.PS_USER,
+  password: process.env.PS_PASS,
+  database: process.env.PS_DB,
   port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
