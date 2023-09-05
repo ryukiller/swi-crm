2804e6119ec5a482cd0ae625bbaf58c5457488b1
ryukiller <rian.rodrigo.webmaster@gmail.com>
2023-09-05 12:00:29 +0200

    updating a lot

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -26,6 +26,10 @@ const tools = [
   { id: 1, name: 'Password generator', href: '/passgen', initial: 'P' }
 ]
 
+const chat = [
+  { id: 1, name: 'Chat', href: '/chat', initial: 'C' }
+]
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
@@ -99,6 +103,36 @@ const Sidebar = () => {
                         {tool.initial}
                       </span>
                       <span className="truncate">{tool.name}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li>
+              <div className="text-xs font-semibold leading-6 text-gray-400">Comunicazioni</div>
+              <ul role="list" className="-mx-2 mt-2 space-y-1">
+                {chat.map((chat) => (
+                  <li key={chat.name}>
+                    <Link
+                      href={chat.href}
+                      className={classNames(
+                        pathname === chat.href
+                          ? 'bg-gray-50 text-indigo-600'
+                          : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
+                        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                      )}
+                    >
+                      <span
+                        className={classNames(
+                          pathname === chat.href
+                            ? 'text-indigo-600 border-indigo-600'
+                            : 'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600',
+                          'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'
+                        )}
+                      >
+                        {chat.initial}
+                      </span>
+                      <span className="truncate">{chat.name}</span>
                     </Link>
                   </li>
                 ))}
