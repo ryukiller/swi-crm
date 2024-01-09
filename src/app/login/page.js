46f26af7ace819d27f7c7385c04c4a96ba2b811e
ryukiller <rian.rodrigo.webmaster@gmail.com>
2024-01-09 12:10:16 +0100

    up

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -12,8 +12,6 @@ export default function LogIn() {
         const result = await signIn("credentials", {
             email: userEmail.current,
             password: pass.current,
-            redirect: true,
-            callbackUrl: "/",
         });
     };
 
